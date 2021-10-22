aed447b93c46f1e46bea7773677a1badea973190
Budi Oktaviyan <budioktaviyans@gmail.com>
2021-10-22 21:25:21 +0700

    Update dependencies version

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -1,17 +1,17 @@
 object Version {
-  const val dagger = "2.38.1"
-  const val anvil = "2.3.3"
-  const val coil = "1.3.1"
+  const val dagger = "2.40.1"
+  const val anvil = "2.3.9"
+  const val coil = "1.4.0"
   const val okhttp = "4.9.1"
 
   object Kotlin {
-    const val stdlib = "1.5.21"
-    const val serialization = "1.2.2"
+    const val stdlib = "1.5.31"
+    const val serialization = "1.3.1"
   }
 
   object AndroidX {
     const val appcompat = "1.3.1"
-    const val constraintlayout = "2.1.0"
+    const val constraintlayout = "2.1.2"
     const val lifecycle = "2.2.0"
   }
 
@@ -21,7 +21,7 @@ object Version {
   }
 
   object ReactiveX {
-    const val rxjava = "3.0.13"
+    const val rxjava = "3.1.2"
     const val rxkotlin = "3.0.1"
     const val rxandroid = "3.0.0"
   }
